5de2ae56c776387d464f3f5ff6f95dfe970bd494
Alexander Mancevice <amancevice@cargometrics.com>
2018-10-25 11:30:52 -0400

    fix test

--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,11 @@
 const assert = require('assert');
 const request = require('supertest');
-const app = require('../index');
+const slackend = require('../index');
+slackend.app.use('/', slackend.router);
 
 describe('GET /', () => {
   it('responds with empty json', (done) => {
-    request(app)
+    request(slackend.app)
       .get('/')
       .set('Accept', 'application/json')
       .expect(200, {}, done);
