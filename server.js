50fdf6232deda18e45cc19f87f379338b2fbbde2
Alexander Mancevice <amancevice@cargometrics.com>
2019-02-05 13:08:27 -0500

    prettify server.js

--- server.js
+++ server.js
@@ -2,8 +2,10 @@
 require('dotenv').config();
 const express  = require('express');
 const slackend = require('./index');
-const port     = process.env.PORT || 3000;
-const host     = process.env.SLACKEND_HOST || 'localhost';
-const baseUrl  = process.env.SLACKEND_BASE_URL || '/';
+
+const PORT     = process.env.PORT              || 3000;
+const HOST     = process.env.SLACKEND_HOST     || 'localhost';
+const BASE_URL = process.env.SLACKEND_BASE_URL || '/';
+
 slackend.app.use(baseUrl, slackend.router);
-slackend.app.listen(port, () => console.log(`> Listening on ${host}:${port}${baseUrl}`));
+slackend.app.listen(port, () => console.log(`> Listening on ${HOST}:${PORT}${BASE_URL}`));
