ba77b1b70f5933d1ca9e981160ac15caad5a8f1f
Alexander Mancevice <amancevice@cargometrics.com>
2019-02-05 13:09:56 -0500

    prettify cloudfunctions.js

--- cloudfunction.js
+++ cloudfunction.js
@@ -1,12 +1,14 @@
 'use strict';
 require('dotenv').config();
+const PubSub   = require('@google-cloud/pubsub');
 const slackend = require('slackend');
+
+const pubsub = new PubSub();
+
 slackend.app.use('/', slackend.router);
 
 slackend.app.set('publish', (payload, topic) => {
-  const PubSub = require('@google-cloud/pubsub');
-  const pubsub = new PubSub();
-  const data   = Buffer.from(JSON.stringify(payload));
+  const data = Buffer.from(JSON.stringify(payload));
   return pubsub.topic(topic).publisher().publish(data);
 });
 
