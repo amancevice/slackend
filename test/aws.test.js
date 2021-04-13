95243931e79c36e4872cb2ee2d99a176d349eba9
Alexander Mancevice <alexander.mancevice@hey.com>
2021-04-12 20:40:38 -0400

    prettier tests

--- test/aws.test.js
+++ test/aws.test.js
@@ -1,73 +1,88 @@
-const assert   = require('assert');
-const express  = require('express');
-const qs       = require('querystring');
-const request  = require('supertest');
-const slackend = require('../aws');
+const assert = require("assert");
+const express = require("express");
+const qs = require("querystring");
+const request = require("supertest");
+const slackend = require("../aws");
 
 const MOCK_SECRET = {
-  SLACK_CLIENT_ID:          '123456789012.123456789012',
-  SLACK_CLIENT_SECRET:      '1234567890abcdef1234567890abcdef',
-  SLACK_OAUTH_INSTALL_URI:  'https://slack.com/oauth/v2/authorize',
-  SLACK_OAUTH_REDIRECT_URI: 'http://localhost:3000/oauth/callback',
-  SLACK_OAUTH_SUCCESS_URI:  'slack://channel?team={TEAM_ID}&id={CHANNEL_ID}',
-  SLACK_SIGNING_VERSION:    'v0',
-  SLACK_TOKEN:              'xoxb-123456789012-abcdefghijklmnopqrstuvwx',
+  SLACK_CLIENT_ID: "123456789012.123456789012",
+  SLACK_CLIENT_SECRET: "1234567890abcdef1234567890abcdef",
+  SLACK_OAUTH_INSTALL_URI: "https://slack.com/oauth/v2/authorize",
+  SLACK_OAUTH_REDIRECT_URI: "http://localhost:3000/oauth/callback",
+  SLACK_OAUTH_SUCCESS_URI: "slack://channel?team={TEAM_ID}&id={CHANNEL_ID}",
+  SLACK_SIGNING_VERSION: "v0",
+  SLACK_TOKEN: "xoxb-123456789012-abcdefghijklmnopqrstuvwx",
 };
 
 const mockRoute = (req, res, next) => {
   res.locals.slack = {
-    ok:      true,
-    team: { id: 'T12345678' },
-    callback_id: 'fizz',
-    incoming_webhook: { channel_id: 'C12345678' },
+    ok: true,
+    team: { id: "T12345678" },
+    callback_id: "fizz",
+    incoming_webhook: { channel_id: "C12345678" },
   };
   next();
 };
 
 const mockSecretsManager = {
   getSecretValue: (options) => {
-    return { promise: () => Promise.resolve({ SecretString: JSON.stringify(MOCK_SECRET) }) };
+    return {
+      promise: () =>
+        Promise.resolve({ SecretString: JSON.stringify(MOCK_SECRET) }),
+    };
   },
 };
 
 const mockEventBridge = {
   putEvents: (options) => {
-    return { promise: () => Promise.resolve('EVENTBRIDGE SUCCESS!') };
+    return { promise: () => Promise.resolve("EVENTBRIDGE SUCCESS!") };
   },
 };
 
 const mockEventBridgeFail = {
   putEvents: (options) => {
-    return { promise: () => Promise.reject('EVENTBRIDGE ERROR!') };
+    return { promise: () => Promise.reject("EVENTBRIDGE ERROR!") };
   },
 };
 
 const mockSlack = {
   chat: {
-    postMessage:   (options) => Promise.resolve(Object.assign(options, { type: 'in_channel' })),
-    postEphemeral: (options) => Promise.resolve(Object.assign(options, { type: 'ephemeral' })),
+    postMessage: (options) =>
+      Promise.resolve(Object.assign(options, { type: "in_channel" })),
+    postEphemeral: (options) =>
+      Promise.resolve(Object.assign(options, { type: "ephemeral" })),
   },
 };
 
-const blockActions   = { type: 'block_actions', actions: [ { action_id: 'my_action' } ] };
-const callback       = { type: 'callback', callback_id: 'my_callback' };
-const viewSubmission = { type: 'view_submission', view: { callback_id: 'my_callback' } };
+const blockActions = {
+  type: "block_actions",
+  actions: [{ action_id: "my_action" }],
+};
+const callback = { type: "callback", callback_id: "my_callback" };
+const viewSubmission = {
+  type: "view_submission",
+  view: { callback_id: "my_callback" },
+};
 
 let app, lambda;
 
-describe('AWS | getApp', function() {
-  before(() => { lambda = slackend({ secretsmanager: mockSecretsManager }); });
+describe("AWS | getApp", function () {
+  before(() => {
+    lambda = slackend({ secretsmanager: mockSecretsManager });
+  });
 
-  it('Creates the app', async function() {
+  it("Creates the app", async function () {
     const app = await lambda.getApp();
     assert.ok(app);
   });
 });
 
-describe('AWS | getEnv', function() {
-  before(() => { lambda = slackend({ secretsmanager: mockSecretsManager }); });
+describe("AWS | getEnv", function () {
+  before(() => {
+    lambda = slackend({ secretsmanager: mockSecretsManager });
+  });
 
-  it('Assigns the secret to process.env', async function() {
+  it("Assigns the secret to process.env", async function () {
     await lambda.getEnv();
     Object.keys(MOCK_SECRET).map((key) => {
       assert.equal(process.env[key], MOCK_SECRET[key]);
@@ -75,113 +90,134 @@ describe('AWS | getEnv', function() {
   });
 });
 
-describe('AWS | getSlack', function() {
-  before(() => { lambda = slackend({ secretsmanager: mockSecretsManager }); });
+describe("AWS | getSlack", function () {
+  before(() => {
+    lambda = slackend({ secretsmanager: mockSecretsManager });
+  });
 
-  it('Creates the Slack client', async function() {
+  it("Creates the Slack client", async function () {
     const slack = await lambda.getSlack();
     assert.equal(slack.token, MOCK_SECRET.SLACK_TOKEN);
   });
 });
 
-describe('AWS | post[Message|Ephemeral]', function() {
-  before(() => { lambda = slackend({ slack: mockSlack }); });
+describe("AWS | post[Message|Ephemeral]", function () {
+  before(() => {
+    lambda = slackend({ slack: mockSlack });
+  });
 
-  it('Calls slack.chat.postMessage', async function() {
-    const msg = { detail: { channel: 'C1234567', text: 'Hello, world!' } };
+  it("Calls slack.chat.postMessage", async function () {
+    const msg = { detail: { channel: "C1234567", text: "Hello, world!" } };
     const ret = await lambda.postMessage(msg);
-    const exp = Object.assign(msg.detail, { type: 'in_channel' });
+    const exp = Object.assign(msg.detail, { type: "in_channel" });
     assert.deepEqual(ret, exp);
   });
 
-  it('Calls slack.chat.postEphemeral', async function() {
-    const msg = { detail: { channel: 'C1234567', text: 'Hello, world!' } };
+  it("Calls slack.chat.postEphemeral", async function () {
+    const msg = { detail: { channel: "C1234567", text: "Hello, world!" } };
     const ret = await lambda.postEphemeral(msg);
-    const exp = Object.assign(msg.detail, { type: 'ephemeral' });
+    const exp = Object.assign(msg.detail, { type: "ephemeral" });
     assert.deepEqual(ret, exp);
   });
 });
 
-describe('AWS | publish', function() {
-  it('Succeeds with 204', function(done) {
+describe("AWS | publish", function () {
+  it("Succeeds with 204", function (done) {
     lambda = slackend({ eventbridge: mockEventBridge });
     app = express();
     app.use(mockRoute, lambda.publish);
-    request(app)
-      .get('/')
-      .set('Accept', 'application/json')
-      .expect(204, done);
+    request(app).get("/").set("Accept", "application/json").expect(204, done);
   });
 
-  it('Fails with 400', function(done) {
+  it("Fails with 400", function (done) {
     lambda = slackend({ eventbridge: mockEventBridgeFail });
     app = express();
     app.use(mockRoute, lambda.publish);
     request(app)
-      .get('/')
-      .set('Accept', 'application/json')
-      .expect(400, 'EVENTBRIDGE ERROR!', done);
+      .get("/")
+      .set("Accept", "application/json")
+      .expect(400, "EVENTBRIDGE ERROR!", done);
   });
 
-  it('OAuth redirects to Slack', function(done) {
+  it("OAuth redirects to Slack", function (done) {
     lambda = slackend({ eventbridge: mockEventBridge });
     app = express();
     app.use(mockRoute, lambda.publish);
     request(app)
-      .get('/oauth/v2')
-      .set('accept', 'application/json')
-      .expect('location', 'slack://channel?team=T12345678&id=C12345678', done);
+      .get("/oauth/v2")
+      .set("accept", "application/json")
+      .expect("location", "slack://channel?team=T12345678&id=C12345678", done);
   });
-})
+});
 
-describe('AWS | handler', function() {
-  it('Succeeds with 204', async function() {
-    lambda = slackend({ secretsmanager: mockSecretsManager, eventbridge: mockEventBridge });
-    const event   = { path: '/slash/fizz', httpMethod: 'POST', body: 'fizz=buzz' };
-    const context = { awsRequestId: 'awsRequestId', succeed: () => {} };
-    const res     = await lambda.handler(event, context);
-    assert.equal(res.statusCode, 204);
-    assert.equal(res.body, '');
-  });
-
-  it('Succeeds with 204 (block_actions)', async function() {
-    lambda = slackend({ secretsmanager: mockSecretsManager, eventbridge: mockEventBridge });
+describe("AWS | handler", function () {
+  it("Succeeds with 204", async function () {
+    lambda = slackend({
+      secretsmanager: mockSecretsManager,
+      eventbridge: mockEventBridge,
+    });
     const event = {
-      path: '/callbacks',
-      httpMethod: 'POST',
-      body: `payload=${ qs.escape(JSON.stringify(blockActions)) }`,
+      path: "/slash/fizz",
+      httpMethod: "POST",
+      body: "fizz=buzz",
+    };
+    const context = { awsRequestId: "awsRequestId", succeed: () => {} };
+    const res = await lambda.handler(event, context);
+    assert.equal(res.statusCode, 204);
+    assert.equal(res.body, "");
+  });
+
+  it("Succeeds with 204 (block_actions)", async function () {
+    lambda = slackend({
+      secretsmanager: mockSecretsManager,
+      eventbridge: mockEventBridge,
+    });
+    const event = {
+      path: "/callbacks",
+      httpMethod: "POST",
+      body: `payload=${qs.escape(JSON.stringify(blockActions))}`,
       headers: {
-        'content-type': 'application/x-www-form-urlencoded'
+        "content-type": "application/x-www-form-urlencoded",
       },
     };
-    const context = { awsRequestId: 'awsRequestId', succeed: () => {} };
-    const res     = await lambda.handler(event, context);
+    const context = { awsRequestId: "awsRequestId", succeed: () => {} };
+    const res = await lambda.handler(event, context);
     assert.equal(res.statusCode, 204);
-    assert.equal(res.body, '');
+    assert.equal(res.body, "");
   });
 
-  it('Succeeds with 204 (view_submission)', async function() {
-    lambda = slackend({ secretsmanager: mockSecretsManager, eventbridge: mockEventBridge });
+  it("Succeeds with 204 (view_submission)", async function () {
+    lambda = slackend({
+      secretsmanager: mockSecretsManager,
+      eventbridge: mockEventBridge,
+    });
     const event = {
-      path: '/callbacks',
-      httpMethod: 'POST',
+      path: "/callbacks",
+      httpMethod: "POST",
       body: `payload=${qs.escape(JSON.stringify(viewSubmission))}`,
       headers: {
-        'content-type': 'application/x-www-form-urlencoded'
+        "content-type": "application/x-www-form-urlencoded",
       },
     };
-    const context = { awsRequestId: 'awsRequestId', succeed: () => {} };
-    const res     = await lambda.handler(event, context);
+    const context = { awsRequestId: "awsRequestId", succeed: () => {} };
+    const res = await lambda.handler(event, context);
     assert.equal(res.statusCode, 204);
-    assert.equal(res.body, '');
+    assert.equal(res.body, "");
   });
 
-  it('Fails with 400', async function() {
-    lambda = slackend({ secretsmanager: mockSecretsManager, eventbridge: mockEventBridgeFail });
-    const event   = { path: '/slash/fizz', httpMethod: 'POST', body: 'fizz=buzz' };
-    const context = { awsRequestId: 'awsRequestId', succeed: () => {} };
-    const res     = await lambda.handler(event, context);
+  it("Fails with 400", async function () {
+    lambda = slackend({
+      secretsmanager: mockSecretsManager,
+      eventbridge: mockEventBridgeFail,
+    });
+    const event = {
+      path: "/slash/fizz",
+      httpMethod: "POST",
+      body: "fizz=buzz",
+    };
+    const context = { awsRequestId: "awsRequestId", succeed: () => {} };
+    const res = await lambda.handler(event, context);
     assert.equal(res.statusCode, 400);
-    assert.equal(res.body, 'EVENTBRIDGE ERROR!');
+    assert.equal(res.body, "EVENTBRIDGE ERROR!");
   });
 });
--- test/index.test.js
+++ test/index.test.js
@@ -1,206 +1,227 @@
-const crypto   = require('crypto');
-const express  = require('express');
-const qs       = require('querystring');
-const request  = require('supertest');
-const slackend = require('../index');
+const crypto = require("crypto");
+const express = require("express");
+const qs = require("querystring");
+const request = require("supertest");
+const slackend = require("../index");
 
 const mockslack = {
   oauth: {
-    access: async (options) => { return {token: 'fizz'}; },
-    v2: { access: async (options) => { return {token: 'fizz'}; }},
-  }
+    access: async (options) => {
+      return { token: "fizz" };
+    },
+    v2: {
+      access: async (options) => {
+        return { token: "fizz" };
+      },
+    },
+  },
 };
 const mockslackerr = {
   oauth: {
-    access: async (options) => { return Promise.reject('BOOM'); },
-    v2: { access: async (options) => { return Promise.reject('BOOM'); }},
-  }
+    access: async (options) => {
+      return Promise.reject("BOOM");
+    },
+    v2: {
+      access: async (options) => {
+        return Promise.reject("BOOM");
+      },
+    },
+  },
 };
 
 const app = (options = {}) => {
-  return slackend(Object.assign({ slack: mockslack }, options))
-    .use((req, res) => res.json(res.locals.slack));
+  return slackend(
+    Object.assign({ slack: mockslack }, options)
+  ).use((req, res) => res.json(res.locals.slack));
 };
 const err = (options = {}) => {
-  return slackend(Object.assign({ slack: mockslackerr, signing_secret: 'fake' }, options))
-    .use((req, res) => res.json(res.locals.slack));
+  return slackend(
+    Object.assign({ slack: mockslackerr, signing_secret: "fake" }, options)
+  ).use((req, res) => res.json(res.locals.slack));
 };
 
-const blockActions   = { type: 'block_actions', actions: [ { action_id: 'my_action' } ] };
-const callback       = { type: 'callback', callback_id: 'my_callback' };
-const viewSubmission = { type: 'view_submission', view: { callback_id: 'my_callback' } };
+const blockActions = {
+  type: "block_actions",
+  actions: [{ action_id: "my_action" }],
+};
+const callback = { type: "callback", callback_id: "my_callback" };
+const viewSubmission = {
+  type: "view_submission",
+  view: { callback_id: "my_callback" },
+};
 
-describe('API | GET /install', function() {
-  it('Responds with 302', function(done) {
+describe("API | GET /install", function () {
+  it("Responds with 302", function (done) {
     request(app())
-      .get('/install')
-      .expect('location', 'https://slack.com/oauth/v2/authorize', done);
+      .get("/install")
+      .expect("location", "https://slack.com/oauth/v2/authorize", done);
   });
-})
+});
 
-describe('API | GET /health', function() {
-  it('Responds OK', function(done) {
-    request(app())
-      .get('/health')
-      .expect(200, { ok: true }, done);
+describe("API | GET /health", function () {
+  it("Responds OK", function (done) {
+    request(app()).get("/health").expect(200, { ok: true }, done);
   });
-})
+});
 
-describe('API | GET /oauth', function() {
-  it('Completes the OAuth workflow', function(done) {
-    let exp = { token: 'fizz' };
+describe("API | GET /oauth", function () {
+  it("Completes the OAuth workflow", function (done) {
+    let exp = { token: "fizz" };
     request(app())
-      .get('/oauth?code=buzz')
-      .set('accept', 'application/json')
+      .get("/oauth?code=buzz")
+      .set("accept", "application/json")
       .expect(200, exp, done);
   });
 
-  it('Redirects to the OAuth error URI', function(done) {
-    request(err({oauth_error_uri: 'https://example.com/error.html'}))
-      .get('/oauth')
-      .set('accept', 'application/json')
-      .expect('location', 'https://example.com/error.html', done);
+  it("Redirects to the OAuth error URI", function (done) {
+    request(err({ oauth_error_uri: "https://example.com/error.html" }))
+      .get("/oauth")
+      .set("accept", "application/json")
+      .expect("location", "https://example.com/error.html", done);
   });
 
-  it('Rejects the OAuth workflow', function(done) {
+  it("Rejects the OAuth workflow", function (done) {
     request(err())
-      .get('/oauth?code=buzz')
-      .set('accept', 'application/json')
+      .get("/oauth?code=buzz")
+      .set("accept", "application/json")
       .expect(403, done);
   });
 });
 
-describe('API | GET /oauth/v2', function() {
-  it('Completes the OAuth workflow', function(done) {
-    let exp = { token: 'fizz' };
+describe("API | GET /oauth/v2", function () {
+  it("Completes the OAuth workflow", function (done) {
+    let exp = { token: "fizz" };
     request(app())
-      .get('/oauth/v2?code=buzz')
-      .set('accept', 'application/json')
+      .get("/oauth/v2?code=buzz")
+      .set("accept", "application/json")
       .expect(200, exp, done);
   });
 
-  it('Redirects to the OAuth error URI', function(done) {
-    request(err({ oauth_error_uri: 'https://example.com/error.html' }))
-      .get('/oauth/v2')
-      .set('accept', 'application/json')
-      .expect('location', 'https://example.com/error.html', done);
+  it("Redirects to the OAuth error URI", function (done) {
+    request(err({ oauth_error_uri: "https://example.com/error.html" }))
+      .get("/oauth/v2")
+      .set("accept", "application/json")
+      .expect("location", "https://example.com/error.html", done);
   });
 
-  it('Redirects to the OAuth error URI', function(done) {
-    request(app({ oauth_error_uri: 'https://example.com/error.html' }))
-      .get('/oauth/v2?error=fizz')
-      .set('accept', 'application/json')
-      .expect('location', 'https://example.com/error.html', done);
+  it("Redirects to the OAuth error URI", function (done) {
+    request(app({ oauth_error_uri: "https://example.com/error.html" }))
+      .get("/oauth/v2?error=fizz")
+      .set("accept", "application/json")
+      .expect("location", "https://example.com/error.html", done);
   });
 
-  it('Rejects the OAuth workflow', function(done) {
+  it("Rejects the OAuth workflow", function (done) {
     request(err())
-      .get('/oauth/v2?code=buzz')
-      .set('accept', 'application/json')
+      .get("/oauth/v2?code=buzz")
+      .set("accept", "application/json")
       .expect(403, done);
   });
 });
 
-describe('API | POST /callbacks', function() {
-  it('Responds with message and topic', function(done) {
+describe("API | POST /callbacks", function () {
+  it("Responds with message and topic", function (done) {
     request(app())
-      .post('/callbacks')
-      .send(`payload=${ qs.escape(JSON.stringify(callback)) }`)
-      .set('accept', 'application/json')
+      .post("/callbacks")
+      .send(`payload=${qs.escape(JSON.stringify(callback))}`)
+      .set("accept", "application/json")
       .expect(200, callback, done);
   });
 
-  it('Responds with message and topic (block_actions)', function(done) {
+  it("Responds with message and topic (block_actions)", function (done) {
     request(app())
-      .post('/callbacks')
-      .send(`payload=${ qs.escape(JSON.stringify(blockActions)) }`)
-      .set('accept', 'application/json')
+      .post("/callbacks")
+      .send(`payload=${qs.escape(JSON.stringify(blockActions))}`)
+      .set("accept", "application/json")
       .expect(200, blockActions, done);
   });
 
-  it('Responds with message and topic (view)', function(done) {
+  it("Responds with message and topic (view)", function (done) {
     request(app())
-      .post('/callbacks')
-      .send(`payload=${ qs.escape(JSON.stringify(viewSubmission)) }`)
-      .set('accept', 'application/json')
+      .post("/callbacks")
+      .send(`payload=${qs.escape(JSON.stringify(viewSubmission))}`)
+      .set("accept", "application/json")
       .expect(200, viewSubmission, done);
   });
 });
 
-describe('API | POST /events', function() {
-  it('Responds with message and topic', function(done) {
-    let exp = { event: { type: 'team_join' }, type: 'event_callback' };
+describe("API | POST /events", function () {
+  it("Responds with message and topic", function (done) {
+    let exp = { event: { type: "team_join" }, type: "event_callback" };
     request(app())
-      .post('/events')
-      .send({ type: 'event_callback', event: { type: 'team_join' } })
-      .set('accept', 'application/json')
+      .post("/events")
+      .send({ type: "event_callback", event: { type: "team_join" } })
+      .set("accept", "application/json")
       .expect(200, exp, done);
   });
 
-  it('Responds with challenge', function(done) {
+  it("Responds with challenge", function (done) {
     request(app())
-      .post('/events')
-      .send({ type: 'url_verification', challenge: 'fizzbuzz' })
-      .set('Accept', 'application/json')
-      .expect(200, { challenge: 'fizzbuzz' }, done);
+      .post("/events")
+      .send({ type: "url_verification", challenge: "fizzbuzz" })
+      .set("Accept", "application/json")
+      .expect(200, { challenge: "fizzbuzz" }, done);
   });
 });
 
-describe('API | POST /slash/:cmd', function() {
-  it('Responds with message and topic', function(done) {
-    let exp = { fizz: 'buzz' };
+describe("API | POST /slash/:cmd", function () {
+  it("Responds with message and topic", function (done) {
+    let exp = { fizz: "buzz" };
     request(app())
-      .post('/slash/fizz')
-      .send('fizz=buzz')
-      .set('Accept', 'application/json')
+      .post("/slash/fizz")
+      .send("fizz=buzz")
+      .set("Accept", "application/json")
       .expect(200, exp, done);
   });
 });
 
-describe('API | Verification', function() {
-  it('Errors with bad signature', function(done) {
+describe("API | Verification", function () {
+  it("Errors with bad signature", function (done) {
     request(err())
-      .post('/callbacks')
-      .send(`payload=${ qs.escape(JSON.stringify(blockActions)) }`)
-      .set('Accept', 'application/json')
-      .expect(403, {error: 'Signatures do not match'}, done);
+      .post("/callbacks")
+      .send(`payload=${qs.escape(JSON.stringify(blockActions))}`)
+      .set("Accept", "application/json")
+      .expect(403, { error: "Signatures do not match" }, done);
   });
 
-  it('Errors with bad timestamp', function(done) {
+  it("Errors with bad timestamp", function (done) {
     request(err())
-      .post('/callbacks')
-      .send(`payload=${ qs.escape(JSON.stringify(blockActions)) }`)
-      .set('Accept', 'application/json')
-      .set('x-slack-request-timestamp', '0')
-      .set('x-slack-signature', 'v0=c340868077bc902f57e4f721a98a957880c7365bea1bb1a9e6fad1a5ebc8ce9c')
-      .expect(403, { error: 'Request too old' }, done);
+      .post("/callbacks")
+      .send(`payload=${qs.escape(JSON.stringify(blockActions))}`)
+      .set("Accept", "application/json")
+      .set("x-slack-request-timestamp", "0")
+      .set(
+        "x-slack-signature",
+        "v0=c340868077bc902f57e4f721a98a957880c7365bea1bb1a9e6fad1a5ebc8ce9c"
+      )
+      .expect(403, { error: "Request too old" }, done);
   });
 
-  it('Skips verification', function(done) {
-    let exp = { fizz: 'buzz' };
-    process.env.SLACK_DISABLE_VERIFICATION = '1';
+  it("Skips verification", function (done) {
+    let exp = { fizz: "buzz" };
+    process.env.SLACK_DISABLE_VERIFICATION = "1";
     request(app())
-      .post('/slash/fizz')
-      .send('fizz=buzz')
-      .set('Accept', 'application/json')
+      .post("/slash/fizz")
+      .send("fizz=buzz")
+      .set("Accept", "application/json")
       .expect(200, exp)
       .then(() => {
         delete process.env.SLACK_DISABLE_VERIFICATION;
         done();
-      })
+      });
   });
 
-  it('Verifies the request', function(done) {
-    let ts   = new Date() / 1000;
-    let hmac = crypto.createHmac('sha256', 'fake');
-    let data = `v0:${ ts }:payload=${ qs.escape(JSON.stringify(blockActions)) }`;
-    let sig  = `v0=${ hmac.update(data).digest('hex') }`;
+  it("Verifies the request", function (done) {
+    let ts = new Date() / 1000;
+    let hmac = crypto.createHmac("sha256", "fake");
+    let data = `v0:${ts}:payload=${qs.escape(JSON.stringify(blockActions))}`;
+    let sig = `v0=${hmac.update(data).digest("hex")}`;
     request(err())
-      .post('/callbacks')
-      .send(`payload=${ qs.escape(JSON.stringify(blockActions)) }`)
-      .set('Accept', 'application/json')
-      .set('x-slack-request-timestamp', `${ ts }`)
-      .set('x-slack-signature', sig)
+      .post("/callbacks")
+      .send(`payload=${qs.escape(JSON.stringify(blockActions))}`)
+      .set("Accept", "application/json")
+      .set("x-slack-request-timestamp", `${ts}`)
+      .set("x-slack-signature", sig)
       .expect(200, blockActions, done);
   });
 });
